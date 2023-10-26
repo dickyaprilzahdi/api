57e00dbacb3ce69e1cf57721651fdb8ec0684f21
Dicky April Zahdi <credentialreuse@gmail.com>
2023-10-27 00:05:55 +0700

    Membuat file index.js dan berhasil melakukan registrasi

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+var morgan = require('morgan');
 const app = express();
 
 // Parsing Aplikasi/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // Panggil routes
 var routes = require('./routes');
 routes(app);
+
+// Daftarkan menu route dari index
+app.use('/auth', require('./middleware'));
 
 app.listen(3000, () => {
     console.log(`Server berjalan dengan normal di port 3000`)
