364f56d24b4d130397f0131eb4fb6a7ade08a85d
Dicky April Zahdi <credentialreuse@gmail.com>
2023-10-26 14:03:22 +0700

    Berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = (function(req, res){
-    response.ok("Aplikasi berjalan dengan normal, silakan lanjutkan ke halaman berikutnya")
+    response.ok("Aplikasi berjalan dengan normal, silakan lanjutkan ke halaman berikutnya", res)
 })
+
+// Menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req, res) {
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    })
+}
--- koneksi.js
+++ koneksi.js
@@ -5,7 +5,7 @@ const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'aplikasijs'
+    database: 'db_aplikasijs'
 });
 
 conn.connect((err) => {
