657e1a4709c7f3aeb0a58bd4b58b48493ca8e9ba
Dicky April Zahdi <credentialreuse@gmail.com>
2023-10-27 10:43:30 +0700

    Berhasil membuat login user token

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ var config = require('../config/secret');
 var ip = require('ip');
 
 // Controller untuk register
-exports.registrasi = function(req, res) {
+exports.registrasi = function (req, res) {
     var post = {
         username: req.body.username,
         email: req.body.email,
@@ -21,17 +21,17 @@ exports.registrasi = function(req, res) {
 
     query = mysql.format(query, table);
 
-    connection.query(query, function(error, rows){
-        if(error){
+    connection.query(query, function (error, rows) {
+        if (error) {
             console.log(error);
         } else {
-            if(rows.length == 0) {
+            if (rows.length == 0) {
                 var query = "INSERT INTO ?? SET ?";
                 var table = ["user"];
 
                 query = mysql.format(query, table);
-                connection.query(query, post, function(error, rows){
-                    if(error) {
+                connection.query(query, post, function (error, rows) {
+                    if (error) {
                         console.log(error);
                     } else {
                         response.ok("Berhasil menambahkan user data baru", res);
@@ -43,3 +43,56 @@ exports.registrasi = function(req, res) {
         }
     })
 }
+
+// Controller untuk login
+exports.login = function (req, res) {
+    var post = {
+        password: req.body.password,
+        email: req.body.email
+    }
+
+    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+    var table = ["user", "password", md5(post.password), "email", post.email];
+
+    query = mysql.format(query, table);
+
+    connection.query(query, function (error, rows) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 1) {
+                var token = jwt.sign ({ rows }, config.secret, {
+                    expiresIn: 1440
+                });
+
+                id_user = rows[0].id;
+
+                var data = {
+                    id_user: id_user,
+                    access_token: token,
+                    ip_address: ip.address()
+                }
+
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["akses_token"];
+
+                query = mysql.format(query, table); 
+                connection.query(query, data, function(error, rows) {
+                    if(error) {
+                        console.log(error);
+                    } else {
+                        res.json({
+                            success: true, 
+                            message: 'Token bernhasil tergenerate!',
+                            token: token, 
+                            currUser: data.id_user
+                        })
+                    }
+                })
+            } else {
+                res.json({"Error": true, "Message": "Email atau password salah!"});
+            }
+        }
+    })
+}
+
